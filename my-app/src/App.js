808921e656941d9be71446178dea6f69770eef08
Diego Henrique de Abreu <abreu.diego94@gmail.com>
2018-05-06 15:11:27 -0300

    no message

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get startsded, edit <code>src/App.js</code> an .          d save to reload.
+          To get startsded, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
     );
